877621ddbaaa578a0067d50459f305eb90142f3a
Colin Woodward <woodwardwebdev@gmail.com>
2020-10-09 16:33:36 +0100

    removed log statement

--- pages/blog.js
+++ pages/blog.js
@@ -8,23 +8,16 @@ import styles from '../styles/Blog.module.css'
 
 
 export default function Blog({ props }) {
-    console.log(props.results)
     return (
         <div className={styles.container}>
             <Header />
             <div className={styles.blogsContainer}>
                 <div className={styles.leftCol}>
                     {props.results.map((blog) => (
-                        // <div className={styles.blogCard}>
-                        //     <h3>{blog.data.title[0].text}</h3>
-                        //     <img src={blog.data.blog_image.url} height='400px' />
-                        //     <p>{blog.data.blog_content[0].text}</p>
-                        // </div>
                         <BlogCard
                             img={blog.data.blog_image.url}
                             title={blog.data.title[0].text}
                             text={blog.data.blog_content[0].text} />
-
                     ))}
                 </div>
                 <div className={styles.rightCol}>
